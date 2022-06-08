b6f36cfb247e21fcba1be7ecdaa273b0006beb7a
Adolfo Ramos <ingadolfo@icloud.com>
2022-06-07 22:22:17 -0500

    Add project files.

---
+++ FSYRecords/FSYRecords/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FSYRecords.Models;
+using FSYRecords.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FSYRecords.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
